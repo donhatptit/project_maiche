289c8dd6453ab11c69ff286ac60b7450bbe9bc11
donhatptit <dvnit04@gmail.com>
2019-10-05 10:30:02 +0700

    update file

--- public/js/index.js
+++ public/js/index.js
@@ -63,40 +63,24 @@ $('.slick-list').slick({
 });
 
 $('.slick-list-post').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
 });
+$(".search_click").click(function(){
+    $(".search__overlay").css('display', 'inline-block');
+});
+$(".search__close").click(function () {
+    $(".search__overlay").css('display', 'none');
+});
+
+// $(document).ready(function(){
+//     $('.dropdown a.submenu').on("click", function(e){
+//         $(this).next('ul').toggle();
+//         e.stopPropagation();
+//         e.preventDefault();
+//     });
+// });
 
 
 
